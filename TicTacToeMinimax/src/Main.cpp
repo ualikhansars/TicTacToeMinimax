c8284363e0fb30fb352406b42946309f7b609e65
Ualikhan Sarsenbayev <ualikhansars@gmail.com>
2018-12-21 08:22:58 -0800

    setup glew

--- TicTacToeMinimax/src/Main.cpp
+++ TicTacToeMinimax/src/Main.cpp
@@ -1,4 +1,7 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -18,6 +21,10 @@ int main(void)
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+
+	/* Initialize GLEW */
+	if (glewInit() != GLEW_OK)
+		std::cout << "error while initializing glew" << std::endl;
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
